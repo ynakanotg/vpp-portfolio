1eb74817e71413e5006b980803202095555d8477
ynakanotg <ynakano@tokyo-gas.co.jp>
2025-10-09 14:56:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// 画像をフェードイン
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.querySelector(".fade-in");
+  setTimeout(() => {
+    img.classList.add("visible");
+  }, 500);
+});
+
+// ボタンのクリックイベント
 function showAlert() {
   alert("VPPは再生可能エネルギーの活用を促進します！");
 }
